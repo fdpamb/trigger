303eb2b8a48b31d8bd58c266ce9ff0e88973e031
Friska (Dendi) Pambudi <friska.pambudi@gridqube.com>
2025-04-03 15:13:12 +1000

    fixed eslint errors on @typescript-eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
-  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  extends: [
+  'eslint:recommended', 'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  'plugins': [
+    '@typescript-eslint'
+  ],
   rules: {
     'default-case': 0,
     'import/no-extraneous-dependencies': 0,
     'react-hooks/exhaustive-deps': 0,
     'react/no-find-dom-node': 0,
     'react/no-did-update-set-state': 0,
-    'react/no-unused-state': 1,
+    'react/no-unused-state': 0,
     'react/sort-comp': 0,
     'jsx-a11y/label-has-for': 0,
     'jsx-a11y/label-has-associated-control': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-unused-vars': 0,
   },
 };
