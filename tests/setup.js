6b2da1e651b0be5ce71b26cf392e3549aa10409b
Friska (Dendi) Pambudi <friska.pambudi@gridqube.com>
2025-04-04 16:09:50 +1000

    added a mock structuredClone

--- tests/setup.js
+++ tests/setup.js
@@ -1,3 +1,6 @@
 // jsdom add motion events to test CSSMotion
 window.AnimationEvent = window.AnimationEvent || (() => {});
 window.TransitionEvent = window.TransitionEvent || (() => {});
+global.structuredClone = jest.fn(val => {
+    return JSON.parse(JSON.stringify(val));
+});
